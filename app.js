5686532b61276ed02e957652d79d4c1ce5217f23
Mahtab Alam <smmalam9@gmail.com>
2020-05-13 21:46:19 +0530

    todo

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+
+// adding into todos
+
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const generateTemplate = (todo) => {
+  const html = ` <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>`;
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if (todo.length === 0) {
+    alert("Please Enter a value ");
+  } else {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+//delete todos----
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
